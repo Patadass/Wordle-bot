6407f8e64ba07cdb7c181593dab6e8ca561e5049
Patadass <ajdinskiivan1@gmail.com>
2023-11-13 11:42:10 +0100

    Trying to commit
    Signed-off-by: Patadass <ajdinskiivan1@gmail.com>

---
+++ Wordle_bot/cmake-build-debug/CMakeFiles/Wordle_bot.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Wordle_bot.dir/main.cpp.obj"
+  "CMakeFiles/Wordle_bot.dir/main.cpp.obj.d"
+  "Wordle_bot.exe"
+  "Wordle_bot.exe.manifest"
+  "Wordle_bot.pdb"
+  "libWordle_bot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Wordle_bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Wordle_bot/cmake-build-debug/CMakeFiles/Wordle_bot.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Wordle_bot.
+# This may be replaced when dependencies are built.

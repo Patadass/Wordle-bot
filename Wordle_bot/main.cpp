d3652f49f9315a764caf56d72b8d1cbd76a12875
Patadass <ajdinskiivan1@gmail.com>
2023-11-21 19:23:46 +0100

    -Made code more readable -Put everything in own functions -Made code less spaghetti
    Signed-off-by: Patadass <ajdinskiivan1@gmail.com>

--- Wordle_bot/main.cpp
+++ Wordle_bot/main.cpp
@@ -8,8 +8,50 @@
 
 using namespace std;
 
+void getDictionary(vector<string>& listOfWords){//this handles putting the hole wordle dictionary in vector<string> listOfWords
+    string line;
+    fstream file;
+    file.open("dictionary/wordle-La.txt");
+    if(!file.is_open()){
+        cout<<"COULDN'T OPEN wordle-La.txt"<<endl;
+    }
+    for(int i = 0; getline(file,line);i++){
+        listOfWords.push_back(line);
+    }
+    file.close();
+}
 
-vector<string> getNewLetters(vector<string>& letters){
+void saveToFile(vector<string>& saveWord,int counter_for_over,vector<string>& saveLetterColor,bool is_over){//this handles saving the attempt to "all_attempts.txt"
+    fstream file;
+    auto start = std::chrono::system_clock::now();
+    auto end = std::chrono::system_clock::now();
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    //ctime(&end_time)
+    file.open("saves/all_attempts.txt", ios::app);
+    if(!file.is_open()){
+        cout<<"COULD NOT OPEN SAVE FILE"<<endl;
+        cout<<"ATTEMPT HAS NOT BEEN SAVED"<<endl;
+        return;
+    }
+    if(is_over){
+        file<<"------------------WON-----------------------"<<endl;
+        file<<"GUESSED THE WORD "<<saveWord.back()<<endl;
+        file<<"IN "<<++counter_for_over<<" GUESSES"<<endl;
+    }else{
+        file<<"------------------LOST-----------------------"<<endl;
+    }
+    file<<"TIME OF GUESSING:"<<ctime(&end_time)<<endl;
+    for(int i = 0;i < saveWord.size();i++){
+        file<<saveWord[i]<<endl;
+        file<<saveLetterColor[i]<<endl;
+        file<<endl;
+    }
+    file<<"--------------------------------------------"<<endl;
+    file.close();
+    cout<<"ATTEMPT SAVED SUCCESSFULLY"<<endl;
+}
+
+vector<string> getNewLetters(vector<string>& letters){//this is for inputting the letter colors that are given by wordle
     string letter;
     for(int i = 0;i < letters.size();i++){
         cout<<letters[i];
@@ -25,7 +67,7 @@ vector<string> getNewLetters(vector<string>& letters){
     return letters;
 }
 
-string toLowerCase(string text){
+string toLowerCase(string text){//basic algorithm to convert string to lower case
     for(int i = 0;i < text.size();i++){
         if(text[i] >= 'A' && text[i] <= 'Z'){
             text[i] += ' ';
@@ -34,7 +76,7 @@ string toLowerCase(string text){
     return text;
 }
 
-int isLineAllowed(string line){
+int isLineAllowed(string line){//this is for the first word input if user want to put in their own word checks if that word is allowed
     if(line.size() != 5){
         return 1;
     }
@@ -64,44 +106,26 @@ int isLineAllowed(string line){
     return 0;
 }
 
-void wordle_bot(vector<string>& listOfWords,string line,const string& wantSave){
-    /*todo:make algorithm better
-     * 1.it gets confused when there is a yellow and grey of the same color it still works but not efficiently
-     * 2.maybe if there are 4 greens it should instead of continuing to guess with those 4 letters-
-     * -it should guess once with the remaining letters so that it eliminates more letters and makes the word easier to guess-
-     * -instead of just going through the list and trying to guess every word with those 4 letters*/
-
-    fstream file;
-    bool can_skip = false;
-    bool is_over = false;
-
-    int counter_for_over = 0,size_of_a_word = 5;
-
-    string letter;
-
-    vector<string> saveWord;
-    vector<string> saveLetterColor;
-
-    vector<char> not_letters;
-    vector<int> not_letters_pos;
-
-    vector<string> letters = {"a","l","e","r","t"};
-    letters[0] = line[0];
-    letters[1] = line[1];
-    letters[2] = line[2];
-    letters[3] = line[3];
-    letters[4] = line[4];
-
-    saveWord.emplace_back(line);
-
-    letters = getNewLetters(letters);
-    string letterColor;
-    for(int j = 0;j < letters.size();j++){
-        letter = letters[j];
+bool SaveLetterColor(vector<string>& saveLetterColor,vector<string>& letters){//this is to save the color of the letters
+    string letter,letterColor;
+    bool is_over = true;
+    for(int i = 0; i < letters.size();i++){
+        letter = letters[i];
         letterColor += letter[1];
-        if(j != 0)saveLetterColor.pop_back();
-        saveLetterColor.push_back(letterColor);
+        if(i + 1 >= letters.size()){
+            saveLetterColor.push_back(letterColor);
+        }
+        if(letter[1] != 'G'){
+            is_over = false;
+        }
     }
+    return is_over;
+}
+
+void NotLetters(vector<char>& not_letters,vector<int>& not_letters_pos,vector<string>& letters){//this is to sore the letters that are not in the word
+    int size_of_a_word = 5;
+    string line,letter;
+    bool can_skip = false;
     for(int j = 0;j < size_of_a_word;j++){
         line = letters[j];
         for(int k = 0;k < size_of_a_word;k++){
@@ -118,129 +142,144 @@ void wordle_bot(vector<string>& listOfWords,string line,const string& wantSave){
             not_letters_pos.push_back(j);
         }
     }
+}
+
+bool WordCheckingALG(vector<string>& letters,string line){//main algorithm for checking if a word is compatible (CAN BE MASSIVELY IMPROVED)
+    /*todo:make algorithm better
+     * 1.it gets confused when there is a yellow and grey of the same color it still works but not efficiently
+     * 2.maybe if there are 4 greens it should instead of continuing to guess with those 4 letters-
+     * -it should guess once with the remaining letters so that it eliminates more letters and makes the word easier to guess-
+     * -instead of just going through the list and trying to guess every word with those 4 letters*/
+    int size_of_a_word = 5;
+    string letter;
+    bool can_skip = false;
+    for(int j = 0;j < size_of_a_word;j++){
+        letter = letters[j];
+
+        if(letter[1] == 'N')continue;//if it's an N we don't check anything
+
+        if(letter[1] == 'G'){//if letter color is G than we want a word that has a letter in that exact position
+            if(line[j] != letter[0]){
+                can_skip = true;
+                break;
+            }
+        }
+
+        if(letter[1] == 'Y'){//if letter color is Y than we want a word with that letter in it but not in the same position
+            bool has_letter = false;
+            for(int k = 0;k < size_of_a_word;k++){
+                if(k == j){// we don't want to check for the position where there was a yellow
+                    if(line[k] != letter[0]){//if there is the same letter in the same position we skip that word
+                        continue;
+                    }
+                    break;
+                }
+                if(line[k] == letter[0]){
+                    has_letter = true;
+                    break;
+                }
+            }
+
+            if(!has_letter){
+                can_skip = true;
+                break;
+            }
+        }
+    }
+    return can_skip;
+}
+
+bool CheckNotLetters(vector<char>& not_letters,string line){//this checks if any of the letters of line are in not_letters if so than it skips that word
+    bool can_skip = false;
+    for(int j = 0;j < line.size();j++){
+        for(int k = 0;k < not_letters.size();k++){
+            if(line[j] == not_letters[k]){
+                can_skip = true;
+                break;
+            }
+        }
+        if(can_skip)break;
+    }
+    return can_skip;
+}
+
+
+//vector<string>& not_letters,string line
+
+//vector<string>& letters,string line
+void wordle_bot(vector<string>& listOfWords,string line,const string& wantSave){//main hub of program this is where all other funcs are called
+    bool is_over = false;
+
+    int counter_for_over = 0;
+
+    vector<char> not_letters;
+    vector<int> not_letters_pos;
+
+    vector<string> saveWord;
+    vector<string> saveLetterColor;
+    vector<string> letters = {"a","l","e","r","t"};
+
+    letters[0] = line[0];
+    letters[1] = line[1];
+    letters[2] = line[2];
+    letters[3] = line[3];
+    letters[4] = line[4];
+
+    saveWord.emplace_back(line);
+
+    letters = getNewLetters(letters);
+
+    SaveLetterColor(saveLetterColor,letters);
+
+    NotLetters(not_letters,not_letters_pos,letters);
 
     for(int i = 0;i < listOfWords.size();i++){
-        can_skip = false;
+
         line = listOfWords[i];
+
         listOfWords[i] = " ";
         if(line == " "){
             continue;
         }
-        for(int j = 0;j < line.size();j++){
-            for(int k = 0;k < not_letters.size();k++){
-                if(line[j] == not_letters[k]){
-                    can_skip = true;
-                    break;
-                }
-            }
-            if(can_skip)break;
-        }
-        if(can_skip)continue;
-        for(int j = 0;j < size_of_a_word;j++){
-            letter = letters[j];
-            if(letter[1] == 'N')continue;
-            if(letter[1] == 'G'){
-                if(line[j] != letter[0]){
-                    can_skip = true;
-                    break;
-                }
-            }
-            if(letter[1] == 'Y'){
-                bool has_letter = false;
-                for(int k = 0;k < size_of_a_word;k++){
-                    if(k == j){//this made algorithm a bit better but TODO:if yellow is in the position don't let any future words have that letter in that position
-                        if(line[k] != letter[0]){
-                            continue;
-                        }
-                        break;
-                    }
-                    if(line[k] == letter[0]){
-                        has_letter = true;
-                        break;
-                    }
-                }
-                if(!has_letter){
-                    can_skip = true;
-                    break;
-                }
-            }
-        }
-        if(can_skip)continue;
+
+        if(CheckNotLetters(not_letters,line))continue;
+
+        if(WordCheckingALG(letters,line))continue;
+
         counter_for_over++;
         if(counter_for_over > 5){
             counter_for_over--;
             break;
         }
+
         saveWord.push_back(line);
+
         letters[0] = line[0];
         letters[1] = line[1];
         letters[2] = line[2];
         letters[3] = line[3];
         letters[4] = line[4];
+
         letters = getNewLetters(letters);
-        is_over = true;
-        letterColor = "";
-        for(int j = 0;j < letters.size();j++){
-            letter = letters[j];
-            letterColor += letter[1];
-            if(j != 0)saveLetterColor.pop_back();
-            saveLetterColor.push_back(letterColor);
-            if(letter[1] != 'G'){
-                is_over = false;
-            }
-        }
+
+        is_over = SaveLetterColor(saveLetterColor,letters);
+
         if(is_over)break;
+
         i = 0;
-        for(int j = 0;j < size_of_a_word;j++){
-            can_skip = false;
-            line = letters[j];
-            for(int k = 0;k < size_of_a_word;k++){
-                letter = letters[k];
-                if(letter[0] == line[0]){
-                    if(letter[1] == 'Y' || letter[1] == 'G'){
-                        can_skip = true;
-                    }
-                }
-            }
-            if(can_skip)continue;
-            if(line[1] == 'N'){
-                not_letters.push_back(line[0]);
-                not_letters_pos.push_back(j);
-            }
-        }
+
+        NotLetters(not_letters,not_letters_pos,letters);
     }
+
     if(wantSave == "yes"){
-        auto start = std::chrono::system_clock::now();
-        auto end = std::chrono::system_clock::now();
-        std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-        //ctime(&end_time)
-        file.open("saves/all_attempts.txt", ios::app);
-        if(!file.is_open()){
-            cout<<"COULD NOT OPEN SAVE FILE"<<endl;
-            cout<<"ATTEMPT HAS NOT BEEN SAVED"<<endl;
-            return;
-        }
-        if(is_over){
-            file<<"------------------WON-----------------------"<<endl;
-            file<<"GUESSED THE WORD "<<saveWord.back()<<endl;
-            file<<"IN "<<++counter_for_over<<" GUESSES"<<endl;
-        }else{
-            file<<"------------------LOST-----------------------"<<endl;
-        }
-        file<<"TIME OF GUESSING:"<<ctime(&end_time)<<endl;
-        for(int i = 0;i < saveWord.size();i++){
-            file<<saveWord[i]<<endl;
-            file<<saveLetterColor[i]<<endl;
-            file<<endl;
-        }
-        file<<"--------------------------------------------"<<endl;
-        file.close();
-        cout<<"ATTEMPT SAVED SUCCESSFULLY"<<endl;
+        saveToFile(saveWord,counter_for_over,saveLetterColor,is_over);
     }
 }
 
-int main() {
-    string line,wantSave,wantRandom;
+void startInput(vector<string>& listOfWords,string& line,string& wantSave){//this is for the beginning of the program for saveInfo and getting the first word
+    //First inputs
+    //for saves first word and getting dictionary
+    string wantRandom;
     cout<<"Save attempt (yes) (no):";
     cin>>wantSave;
     cout<<"Start with random word (yes) (no):";
@@ -255,18 +294,7 @@ int main() {
         wantOwnWord = toLowerCase(wantOwnWord);
     }
 
-    fstream file;
-
-    vector<string> listOfWords;
-
-    file.open("dictionary/wordle-La.txt");
-    if(!file.is_open()){
-        cout<<"COULDN'T OPEN wordle-La.txt"<<endl;
-    }
-    for(int i = 0; getline(file,line);i++){
-        listOfWords.push_back(line);
-    }
-    file.close();
+    getDictionary(listOfWords);
 
     /*todo:random stopped working for some reason
      * update:random now only works in clion compiler*/
@@ -299,9 +327,12 @@ int main() {
             line = "alert";
         }
     }
+}
 
-
+int main() {
+    vector<string> listOfWords;
+    string line,wantSave;
+    startInput(listOfWords,line,wantSave);
     wordle_bot(listOfWords,line,wantSave);
-
     return 0;
 }
